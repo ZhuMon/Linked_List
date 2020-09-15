5d864deafae61a6e59099ce04c4c36c9b2a9848c
ZhuMon <richardlin0212@gmail.com>
2020-09-15 15:05:42 +0800

    Use pointer to pointer
    Replace ugly code by pointer to pointer

--- main.c
+++ main.c
@@ -6,62 +6,70 @@ typedef struct __node {
     struct __node *next;
 } node_t;
 
-void add_entry(node_t **head, int new_value) {
+void add_entry(node_t **head, int new_value)
+{
     node_t **indirect = head;
 
     node_t *new_node = malloc(sizeof(node_t));
     new_node->value = new_value;
     new_node->next = NULL;
 
-    while (*indirect) indirect = &(*indirect)->next;
+    while (*indirect)
+        indirect = &(*indirect)->next;
     *indirect = new_node;
 }
 
-node_t *find_entry(node_t *head, int value) {
+node_t *find_entry(node_t *head, int value)
+{
     node_t *current = head;
     for (; current && current->value != value; current = current->next)
         /* interate */;
     return current;
 }
 
-void remove_entry(node_t **head, node_t *entry) {
+void remove_entry(node_t **head, node_t *entry)
+{
     node_t **indirect = head;
 
-    while ((*indirect) != entry) indirect = &(*indirect)->next;
+    while ((*indirect) != entry)
+        indirect = &(*indirect)->next;
 
     *indirect = entry->next;
     free(entry);
 }
 
-node_t *swap_pair(node_t *head) {
-    for (node_t **node = &head; *node && (*node)->next;
+void swap_pair(node_t **head)
+{
+    for (node_t **node = head; *node && (*node)->next;
          node = &(*node)->next->next) {
         node_t *tmp = *node;
         *node = (*node)->next;
         tmp->next = (*node)->next;
         (*node)->next = tmp;
     }
-    return head;
 }
 
-node_t *reverse(node_t *head) {
+void reverse(node_t **head)
+{
     node_t *cursor = NULL;
-    while (head) {
-        node_t *next = head->next;
-        head->next = cursor;
-        cursor = head;
-        head = next;
+    while (*head) {
+        node_t *next = (*head)->next;
+        (*head)->next = cursor;
+        cursor = *head;
+        *head = next;
     }
-    return cursor;
+    *head = cursor;
 }
 
-void print_list(node_t *head) {
+void print_list(node_t *head)
+{
     for (node_t *current = head; current; current = current->next)
         printf("%d ", current->value);
     printf("\n");
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     node_t *head = NULL;
 
     print_list(head);
@@ -86,7 +94,7 @@ int main(int argc, char const *argv[]) {
     /* swap pair.
      * See https://leetcode.com/problems/swap-nodes-in-pairs/
      */
-    head = swap_pair(head);
+    swap_pair(&head);
     print_list(head);
 
     reverse(&head);
